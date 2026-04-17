e03fc9a6614ebb7f545893697ab5f636166adb53
nefeliatmn <nefeliatmn@gmail.com>
2026-04-17 18:56:29 +0300

    yried connecting the logic to the index

---
+++ js/index.js
@@ -0,0 +1,2 @@
+import { playGame } from "./logic.js";
+game.playGame();
--- js/logic.js
+++ js/logic.js
@@ -1,9 +1,90 @@
+const choices = ["rock", "paper", "scissors"];
 
-export const choices = ["Rock", "Paper", "Scissors"];
-
-export const computerPlay = () => {
+const computerPlay = () => {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-// Delete the code below whe finished with testing
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+  if (!playerSelection || typeof playerSelection !== "string") {
+    return "Invalid input, round void!";
+  }
+
+  let playerChoice = playerSelection.toLowerCase();
+  let computerChoice = computerSelection.toLowerCase();
+
+  if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+    return "Invalid input, round void!";
+  }
+
+  if (playerChoice === computerChoice) {
+    return "It's a tie!";
+  }
+
+  if (playerChoice === "rock") {
+    if (computerChoice === "scissors") {
+      return "You win! Rock beats scissors";
+    } else {
+      return "You lose! Paper beats rock";
+    }
+  }
+
+  if (playerChoice === "paper") {
+    if (computerChoice === "rock") {
+      return "You win! Paper beats rock";
+    } else {
+      return "You lose! Scissors beats paper";
+    }
+  }
+
+  if (playerChoice === "scissors") {
+    if (computerChoice === "paper") {
+      return "You win! Scissors beats paper";
+    } else {
+      return "You lose! Rock beats scissors";
+    }
+  }
+}
+
+
+let playerScore, computerScore;
+
+function playGame() {
+  playerScore = 0;
+  computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const computerSelection = computerPlay();
+    const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    const result = playRound(playerSelection, computerSelection);
+    console.log(result);
+    if (result.includes("win")) {
+      playerScore++;
+    } else if (result.includes("lose")) {
+      computerScore++;
+    }
+
+  }
+  console.log(`Final Score: Player ${playerScore} :  Computer ${computerScore}`);
+  if (playerScore > computerScore) {
+    console.log("Congratulations! You won the game!");
+  }
+  else if (playerScore < computerScore) {
+    console.log("Sorry, you lost the game!");
+  }
+  else {
+    console.log("The game is a tie!");
+  }
+
+  playAgain();
+}
+
+function playAgain() {
+  const playAgain = confirm("Do you want to play again?");
+  if (playAgain) {
+    playGame();
+  }
+  else {
+    console.log("Thanks for playing!");
+  }
+}
+export { playGame };
